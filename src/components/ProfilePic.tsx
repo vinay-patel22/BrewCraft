32eeee5ce0a0b3cc943a903f36a8167e86f4389e
Vinay Patel <vinaypatel5519@gmai.com>
2024-02-16 21:46:38 +0530

    Start HomeScreen

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Image, View } from 'react-native';
+import { COLORS, SPACING } from '../theme/theme';
 
-export default function ProfilePic() {
+const ProfilePic = () => {
   return (
-    <View>
-      <Text>ProfilePic</Text>
+    <View style={styles.ImageContainer}>
+      <Image
+        source={require('../assets/app_images/avatar.png')}
+        style={styles.Image}
+      />
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ImageContainer: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+    borderRadius: SPACING.space_12,
+    borderWidth: 2,
+    borderColor: COLORS.secondaryDarkGreyHex,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  Image: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+  },
+});
+
+export default ProfilePic;
